7cd79ac24e63856672607959f99409a71ded6593
P. Colin Manikoth <pmanikoth@ewu.edu>
2014-03-17 16:00:35 -0700

    vadsf

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+// Make Foundation Go!
+//$(document).foundation();
+
+// Your Awesome Scripts!
+$(document).ready(function(){
+
+	$('#getWeather button').on('click', function() {
+
+     // 1. Get & store entered zipcode
+  var zipcode = $('#getWeather input').val();
+  
+  // 2. Pass weather into _simpleWeather()_ object
+  $.simpleWeather({
+    
+    location: zipcode,
+  
+    success: function(weather) {
+      
+      // Get & store temperature
+      var temp = weather.temp;
+      // Get & store city
+      var city = weather.city;
+      
+      // Output to hooks in HTML
+      $('.temp').text(temp);
+      $('.city').text(city);
+
+      // See console for all properties of object
+      console.log(weather);
+    },
+  
+    error: function(error) {
+      $('body').html('<p>' + error + '</p>');
+    }
+  
+  });
+  
+  // 3. Reset input value
+  $('#getWeather input').val('');
+
+  });
+
+
+
+}); 
